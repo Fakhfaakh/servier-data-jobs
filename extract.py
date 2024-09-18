9a3d69cc721bc520e0a7d85ad588f77e4666fce7
Khaled Fakhfakh <fakhfaakh@gmail.com>
2024-09-18 18:00:29 +0200

    Update joining paths method in extract

--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import os
 """
 Extract data from csv files and return 3 dataframes each corresponding to one file
 in data_path
@@ -7,4 +7,4 @@ in data_path
 data_path = string: Data location, any valid string path is acceptable.
 """
 def extract_data(data_path='./data/'):
-    return pd.read_csv(f'{data_path}/clinical_trials.csv'), pd.read_csv('{data_path}/pubmed.csv'), pd.read_csv('{data_path}/drugs.csv')
+    return pd.read_csv(os.path.join(data_path,'clinical_trials.csv')), pd.read_csv(os.path.join(data_path, 'pubmed.csv')), pd.read_csv(os.path.join(data_path,'drugs.csv'))
