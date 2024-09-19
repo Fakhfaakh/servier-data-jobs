7fea8c39a22bf15514d007d8a718427d4e2bfe27
Khaled Fakhfakh <fakhfaakh@gmail.com>
2024-09-19 08:12:03 +0200

    Update main script after adding ad-hoc questions

--- graph_generator.py
+++ graph_generator.py
@@ -1,7 +1,7 @@
 import argparse
 from utils.load import load_data
 from utils.extract import extract_data
-from utils.transform import clean_data, generate_graph
+from utils.transform import clean_data, generate_graph, extract_journal_with_most_drugs, find_related_drugs_not_in_clinical_trials
 import logging
 
 logger = logging.getLogger(__name__)
@@ -21,6 +21,7 @@ def generate_drugs_graph(data_path_:str, destination_:str):
     # loading data into a json file
     load_data(graph, destination_)
     logger.info("------ DATA SUCCESSFULLY LOADED")
+    return graph
 
 if __name__ == "__main__":
     # setting the logging level to INFO
@@ -31,5 +32,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate drugs JSON graph from publications issued from clinical trials and PubMed publications")
     parser.add_argument('--data_path', required=True, help='Path to the directory containing input CSV files.')
     parser.add_argument('--destination', required=True, help='Destination path for the output JSON file.')
+    parser.add_argument('--bonus', action='store_true', help='Bonus-part--Journal that mentions the most different drugs')
+    parser.add_argument('--target_drug', help='Bonus-part--Specify a drug to get related drugs mentioned by the same journals ref by PubMed but not by clinical trials')
+
     args = parser.parse_args()
-    generate_drugs_graph(args.data_path, args.destination)
+    graph = generate_drugs_graph(args.data_path, args.destination)
+    if args.bonus:
+        logger.info(extract_journal_with_most_drugs(graph))
+    if args.target_drug:
+        logger.info(f'Related drugs are: {find_related_drugs_not_in_clinical_trials(args.target_drug, graph)}')
